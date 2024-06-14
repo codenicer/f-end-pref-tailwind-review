402899b7f0db4f0bee422e4052e09f695172fce0
codeNicer <ruthertenido@gmail.com>
2024-06-14 16:36:35 +0800

    add select component to cart

--- react/tailwind-review/src/component/CartItem.tsx
+++ react/tailwind-review/src/component/CartItem.tsx
@@ -1,4 +1,6 @@
-import { Controller } from '../_utils/constant'
+import { CiTrash } from 'react-icons/ci'
+import { COLOR, Controller, QUANTITY } from '../_utils/constant'
+import { Select } from './Select'
 
 export function CartItem({
   controller,
@@ -6,15 +8,32 @@ export function CartItem({
   controller: Controller
 }): JSX.Element {
   return (
-    <div
-      className={`flex bg-gray-50 cursor-pointer hover:bg-lime-200 hover:bg-opacity-50 space-x-2 py-2`}
-    >
-      <img className="h-24 max-w-24" src={controller.src} />
-      <div className='space-y-2'>
-        <div className="font-bold">{controller.title}</div>
-        <div className="text-small text-gray-400">{controller.description}</div>
+    <div className="cursor-pointer  hover:bg-lime-200  bg-gray-50 hover:bg-opacity-50 p-2 space-y-2">
+      <div className={`flex space-x-2 py-2`}>
+        <img className="h-24 max-w-24" src={controller.src} />
+        <div className="space-y-2">
+          <div className="font-bold">{controller.title}</div>
+          <div className="text-small text-gray-400">
+            {controller.description}
+          </div>
+        </div>
+        <div className="font-bold">{`${controller.price}$`}</div>
       </div>
-      <div className="font-bold">{`${controller.price}$`}</div>
+      <div className="flex justify-between">
+        <div className="flex space-x-6">
+          <div>
+            <div>{'QTY'}</div>
+            <Select  options={QUANTITY}></Select>
+          </div>
+          <div>
+            <div>{'COLOR'}</div>
+            <Select options={COLOR}></Select>
+          </div>
+          <button>
+            <CiTrash size={25} className="text-black" />
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
--- react/tailwind-review/src/component/Select.tsx
+++ react/tailwind-review/src/component/Select.tsx
@@ -1,21 +1,30 @@
-import { IoIosArrowDown } from "react-icons/io"
+import { IoIosArrowDown } from 'react-icons/io'
 
 export function Select({
   options,
   title,
 }: {
-  title: string
+  title?: string
   options: string[]
 }): JSX.Element {
   return (
     <div className="relative ">
-      <select defaultValue={""} className="w-25 appearance-none border border-gray-300 p-4  rounded-sm">
-       <option value="" disabled hidden>{title}</option>
-        {options.map(option=> <option key={option}>{option}</option>)}
+      <select
+        defaultValue={''}
+        className="w-25 appearance-none border border-gray-300 p-4  rounded-sm"
+      >
+        {title && (
+          <option value="" disabled hidden>
+            {title}
+          </option>
+        )}
+        {options.map((option) => (
+          <option key={option}>{option}</option>
+        ))}
       </select>
       <div className="pointer-events-none absolute inset-y-0 right-0 flex-center pr-3">
-         <IoIosArrowDown></IoIosArrowDown>
-        </div>
+        <IoIosArrowDown></IoIosArrowDown>
+      </div>
     </div>
   )
 }
